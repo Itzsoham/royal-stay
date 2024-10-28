16d7e8f053f3acaabcffb78dd9f436cebef98010
kai <137367860+Itzsoham@users.noreply.github.com>
2024-10-28 18:16:16 +0530

    Day 6 error in deploy with git on netlify

--- src/features/bookings/BookingTableOperations.jsx
+++ src/features/bookings/BookingTableOperations.jsx
@@ -1,5 +1,5 @@
-import SortBy from "../../ui/SortBy";
 import Filter from "../../ui/Filter";
+import Sortby from "../../ui/SortBy";
 import TableOperations from "../../ui/TableOperations";
 
 function BookingTableOperations() {
@@ -15,7 +15,7 @@ function BookingTableOperations() {
         ]}
       />
 
-      <SortBy
+      <Sortby
         options={[
           { value: "startDate-desc", label: "Sort by date (recent first)" },
           { value: "startDate-asc", label: "Sort by date (earlier first)" },
